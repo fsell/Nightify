429eab2930d33307e5eaa8a6a226e4c782c791bb
fsell <frederick.sell@anavationllc.com>
2021-05-21 11:16:01 -0400

    Second Commit

--- App.js
+++ App.js
@@ -1,21 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native'
+import * as Location from 'expo-location'
+//import * as Twilio from 'twilio';
 
 export default function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultStyle, setDefaultStyle] = useState(true);
+  const [longitude, setLongitude] = useState(0);
+  const [latitude, setLatitude] = useState(0);
+  /*
+  const accountSid = "ACdd2c7a20c9ed95f9b4ebbcfed5cbe167";
+  const authToken = "9b71fff12f98dd39d23eabfd065bef90";
+  const client = require('twilio')(accountSid, authToken);
+
+  client.messages.create({
+      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+      from: '+15122343172',
+      to: '+18572182296',
+  })
+  .then(message => console.log(message.sid));
+  */
+
+  useEffect(() => {
+    load()
+  }, []);
+
+  async function load() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status != 'granted') {
+        setErrorMessage('Access to location is needed to run the app');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync();
+
+      setLatitude(location.coords.latitude);
+      setLongitude(location.coords.longitude);
+
+      console.log("Here!")
+  }
+
+  function sendMessage() {
+    alert(`Longitude: ${longitude}, Latitude:  ${latitude}`);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      onStartShouldSetResponder={() => {
+        setDefaultStyle(!defaultStyle);
+        load();
+        return true;
+      }}
+      onResponderRelease={ () => {
+        setDefaultStyle(!defaultStyle);
+        sendMessage();
+      } }
+      style={defaultStyle ?
+              styles.containerStyle1 :
+              styles.containerStyle2}>
     </View>
   );
 }
-
+/*<TouchableOpacity
+  onPress={() => alert(`Longitude: ${long}, Latitude:  ${lat}`)}
+  style={styles.button}>
+  <Text style={styles.buttonText}>Press Here!</Text>
+</TouchableOpacity>*/
+//<StatusBar style="auto" />
 const styles = StyleSheet.create({
-  container: {
+  containerStyle1: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'blue',
+    opacity: 1.0,
+  },
+  containerStyle2: {
+    flex: 1,
+    backgroundColor: 'blue',
+    opacity: 0.2,
   },
 });
